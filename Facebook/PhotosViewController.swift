7b1264cd1b2c0d246a814467a8a8168b3328a65b
Terra Oldham <toldham@hearsaycorp.com>
2014-11-02 21:16:56 -0800

    Add Done Fade
    Add Done Fade

--- Facebook/PhotosViewController.swift
+++ Facebook/PhotosViewController.swift
@@ -8,20 +8,45 @@
 
 import UIKit
 
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, UIScrollViewDelegate {
     
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
     var image: UIImage!
     
+    @IBOutlet weak var doneImageView: UIImageView!
     @IBAction func onDoneClick(sender: AnyObject) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    func scrollViewDidScroll(scrollView: UIScrollView!) {
+        //var point = scrollView.locationInView(view)
+        //var velocity = scrollView.velocityInView(view)
+        //var translation = scrollView.translationInView(view)
+    }
+    
+    func scrollViewWillBeginDragging(scrollView: UIScrollView!) {
+        UIView.animateWithDuration(0.5, animations: { () -> Void in
+            self.doneImageView.alpha = 0
+        })
+        
+    }
+    
+    func scrollViewDidEndDragging(scrollView: UIScrollView!,
+        willDecelerate decelerate: Bool) {
+            self.doneImageView.alpha = 1
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        scrollView.contentSize = CGSize(width: 320, height: 600)
+        scrollView.delegate = self
+        
 
+        
     }
 
 }
